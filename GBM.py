440f0cef4c574a212574069f478fe349ab67557a
Daksh Save <dakshsave9@gmail.com>
2026-04-19 17:52:00 +0530

    Update GBM.py

--- GBM.py
+++ GBM.py
@@ -8,14 +8,14 @@ Geometric Brownian Motion Formula (Implicit):
 dSt = mu*St*dt + sigma*St*dW
 
 Geometric Brownian Motion Solution (Explicit):
-St = S0*exp((mu - sigma**2 / 2)*t - sigma*Wt)
+St = S0*exp((mu - sigma**2 / 2)*dt - sigma*Wt)
 
 Parameters:
 St = Asset Price At Time t
 S0 = Asset Price At Time t=0 (Initial Price)
 mu = Mean Return / Expected Return (Drift)
 sigma = Volatility (Constant)
-t = Time Periods
+dt = Time Periods
 n = Number Of Time Periods
 T = Number Of Years
 Wt = Standard Brownian Motion (Wiener Process)
@@ -25,25 +25,25 @@ Ns = Number Of Simulations
 
 
 #PARAMETERS
-S0 = _ #Replace the underscore with the Initial Price.
-mu = _ #Replace the underscore with the Mean Return / Expected Return (Drift).
-sigma = _ #Replace the underscore with the Standard Deviation / Volatility.
-n = _ #Replace the underscore with the Number Of Time Periods.
-T = _ #Replace the underscore with the Number Of Years.
-Ns = _ #Replace the underscore with the Number Of Simulations.
-t = T / n
+S0 = 100
+mu = 0.2
+sigma = 0.2
+n = 252
+T = 1
+Ns = 1000
+dt = T / n
 Rs = []
 
 
 #MULTIPLE PRICE PATHS SIMULATION PROCESS
 paths = []
 for sim in range(Ns):
-    S = [S0]
+    St = [S0]
     for step in range(n):
         Z = np.random.normal(0, 1)
-        exponent = (mu - sigma**2 / 2) * t + sigma * np.sqrt(t) * Z
-        S.append(S[-1] * np.exp(exponent))
-    paths.append(S)
+        exponent = (mu - sigma**2 / 2) * dt + sigma * np.sqrt(dt) * Z
+        St.append(St[-1] * np.exp(exponent))
+    paths.append(St)
 
 
 #PATHWISE SIMULATED RETURNS CALCULATION (PERIODIC)
@@ -57,7 +57,7 @@ for path in paths:
 #TIME INTERVAL TO YEARS
 time = []
 for i in range(n + 1):
-    time.append(i * t)
+    time.append(i * dt)
 
 
 #PLOT (SIMULATED PRICES)
